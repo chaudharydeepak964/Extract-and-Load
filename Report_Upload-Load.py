bcdee9e42242108360d7dacca04de6eb98238efb
chaudharydeepak964 <chaudharydeepak964@gmail.com>
2025-09-13 18:47:23 +0530

    Load Code Modifoed by Removing Server Details For Security Purpose

--- Report_Upload-Load.py
+++ Report_Upload-Load.py
@@ -79,391 +79,43 @@ for folder, subfolders,files in os.walk(path):
 print("All Old Raw Files Deleted!")
 
 
-                                   ###########   *********************** CM DOWNLAODING  ***********************  #############
-
-
-
-
-print(" ** CM Files downloading start ** ")
-
-ssh0=paramiko.SSHClient()
-ssh0.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-try:
-    ssh0.connect(hostname='10.115.1.39',username='vodafone',password='Regional@#@#',port=22)
-except:
-    pass
-
-try:
-    ssh0.connect(hostname='10.19.62.239',username='vodafone',password='Regional@#@#',port=22)
-except:
-    pass
-sftp_client0=ssh0.open_sftp()
-
-
-#################  ASM_I  ##################
-
-
-
-#################  UPE  ##################
-
-sftp_client0.chdir('/opt/mycom/data/ceragon/microwave/csvascii_nr21/cm1/UPE' )
-
-try:
-    sftp_client0.get('Full_Link_Report_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_UPE_Full_Link_Report_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Network_Element_Report_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_UPE_Network_Element_Report_'+d1+'.csv')
-except:
-    pass
-
-
-#################  ROB  ##################
-
-sftp_client0.chdir('/opt/mycom/data/ceragon/microwave/csvascii_nr21/cm1/ROB' )
-
-try:
-    sftp_client0.get('Full_Link_Report_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_ROB_Full_Link_Report_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Network_Element_Report_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_ROB_Network_Element_Report_'+d1+'.csv')
-except:
-    pass
-
-
-
-#################  KAR  ##################
-
-
-sftp_client0.chdir('/opt/mycom/data/ceragon/microwave/csvascii_nr21/cm1/KAR' )
-
-try:
-    sftp_client0.get('Full_Link_Report_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_KAR_Full_Link_Report_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Network_Element_Report_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_KAR_Network_Element_Report_'+d1+'.csv')
-except:
-    pass
-
-print(" ** 12 CM Files downloaded ** ")
-
-
-
-
-
-                                   ###########   *********************** PM DOWNLAODING  ***********************  #############
-
-
-
-
-
-
-
-print(" ** PM Files downloading start ** ")
-      
-#################  UPE  ##################
-
-sftp_client0.chdir('/opt/mycom/data/ceragon/microwave/csvascii_nr21/pm1/UPE')
-
-try:
-    sftp_client0.get('Radio_PM_Report_IP-10_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Radio_PM_Report_IP-10_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Radio_PM_Report_IP-20_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Radio_PM_Report_IP-20_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Ethernet_Utilization_Report_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Ethernet_Utilization_Report_24h_'+d1+'.csv')
-except:
-    pass
-
-
-#################  KAR  ##################
-
-
-sftp_client0.chdir('/opt/mycom/data/ceragon/microwave/csvascii_nr21/pm1/KAR' )
-
-
-try:
-    sftp_client0.get('Radio_PM_Report_IP-20_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_KAR_Radio_PM_Report_IP-20_24h_'+d1+'.csv')
-except:
-    pass
-
-
-try:
-    sftp_client0.get('Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_KAR_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Ethernet_Utilization_Report_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_KAR_Ethernet_Utilization_Report_24h_'+d1+'.csv')
-except:
-    pass
-
-
-#################  ROB  ##################
-
-
-sftp_client0.chdir('/opt/mycom/data/ceragon/microwave/csvascii_nr21/pm1/ROB' )
-
-
-try:
-    sftp_client0.get('Radio_PM_Report_IP-20_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Radio_PM_Report_IP-20_24h_'+d1+'.csv')
-except:
-    pass
-
-
-try:
-    sftp_client0.get('Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Radio_PM_Report_IP-10_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Radio_PM_Report_IP-10_24h_'+d1+'.csv')
-except:
-    pass
-
-
-try:
-    sftp_client0.get('Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv')
-except:
-    pass
-
-try:
-    sftp_client0.get('Ethernet_Utilization_Report_24h_'+d1+'.csv',r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Ethernet_Utilization_Report_24h_'+d1+'.csv')
-except:
-    pass
-
-
-
-
-
-sftp_client0.close
-ssh0.close
-
-print(" ** 21 PM Files downloaded ** ")
-
-
-
-
                                                             # ** Reports Uploading **   
 
 
 
-print(" ** Reports Uploading start on BI Portal -->> /home/snenrc/VIL_IDEA_REPORTS/TX_REPORT ** ")
+print(" ** Reports Uploading start on BI Portal -->> /Remote_Path/** ")
 
-print(" ** CM Uploading Start ** ")
+print(" ** Uploading Start ** ")
 
 ssh0=paramiko.SSHClient()
 ssh0.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh0.connect(hostname='10.19.33.41',username='snenrc',password='Qwer!234',port=22)
+ssh0.connect(hostname='10.10.10.10',username='Root',password='Root',port=22)
 sftp_client0=ssh0.open_sftp()
 
 
-###############  FULL LINK  ################
-
 try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_UPE_Full_Link_Report_'+d1+'.csv','TXN_CERA_UPE_Full_Link_Report_'+d1+'.csv')
+    sftp_client0.chdir('\Remote_Path\')
+    sftp_client0.put(r'local_Path\Report_Name_'+d7+'.csv','Report_Name_'+d7+'.csv')
     
 except:
     pass
 
 
 try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_KAR_Full_Link_Report_'+d1+'.csv','TXN_CERA_KAR_Full_Link_Report_'+d1+'.csv')
-    
+    sftp_client0.chdir('\Remote_Path\')
+    sftp_client0.put(r'local_Path\Report_Name_'+d6+'.csv','Report_Name_'+d6+'.csv')
+
 except:
     pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_ROB_Full_Link_Report_'+d1+'.csv','TXN_CERA_ROB_Full_Link_Report_'+d1+'.csv')
-    
-except:
-    pass
-
-
-
-
-
-print(" ** FULL LINK UPLOADED ** ")
-
-
-
-#############  NETOWRK ELEMENT  ##############
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_UPE_Network_Element_Report_'+d1+'.csv','TXN_CERA_UPE_Network_Element_Report_'+d1+'.csv')
-    
-except:
-    pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_KAR_Network_Element_Report_'+d1+'.csv','TXN_CERA_KAR_Network_Element_Report_'+d1+'.csv')
-    
-except:
-    pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\CM\TXN_CERA_ROB_Network_Element_Report_'+d1+'.csv','TXN_CERA_ROB_Network_Element_Report_'+d1+'.csv')
-    
-except:
-    pass
-
-
-print(" ** NETOWRK ELEMENT UPLOADED ** ")
-
-
-print(" ** PM Uploading Start ** ")
-
-#################  UPE  ##################
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Radio_PM_Report_IP-10_24h_'+d1+'.csv','TXN_CERA_UPE_Radio_PM_Report_IP-10_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Radio_PM_Report_IP-20_24h_'+d1+'.csv','TXN_CERA_UPE_Radio_PM_Report_IP-20_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv','TXN_CERA_UPE_Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv','TXN_CERA_UPE_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_UPE_Ethernet_Utilization_Report_24h_'+d1+'.csv','TXN_CERA_UPE_Ethernet_Utilization_Report_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-print(" ** UPE PM UPLOADED ** ")
-
-#################  KAR  ##################
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_KAR_Radio_PM_Report_IP-20_24h_'+d1+'.csv','TXN_CERA_KAR_Radio_PM_Report_IP-20_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_KAR_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv','TXN_CERA_KAR_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_KAR_Ethernet_Utilization_Report_24h_'+d1+'.csv','TXN_CERA_KAR_Ethernet_Utilization_Report_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-print(" ** KAR PM UPLOADED ** ")
-
-
-#################  ROB ##################
-
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Radio_PM_Report_IP-20_24h_'+d1+'.csv','TXN_CERA_ROB_Radio_PM_Report_IP-20_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv','TXN_CERA_ROB_Ethernet_Radio_Report_IP-20_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Radio_PM_Report_IP-10_24h_'+d1+'.csv','TXN_CERA_ROB_Radio_PM_Report_IP-10_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv','TXN_CERA_ROB_Ethernet_Radio_Report_IP-10_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-try:
-    sftp_client0.chdir('/home/snenrc/VIL_IDEA_REPORTS/TX_REPORT')
-    sftp_client0.put(r'C:\Users\COR1736664\Desktop\Deepak\ALL CODE\All_Report_BI_Push\Ceragon\RAW\PM\TXN_CERA_ROB_Ethernet_Utilization_Report_24h_'+d1+'.csv','TXN_CERA_ROB_Ethernet_Utilization_Report_24h_'+d1+'.csv')
-    
-except:
-    pass
-
-
-print(" ** ROB PM UPLOADED ** ")
 
 
 sftp_client0.close
 ssh0.close
 
 
-
 print(" ********* Congratulations All Report Successfully Uploaded *********** ")
 
+
+print('This is a smaple code I deleted the rest of the code lines as all are same only file name changed')
 
 
